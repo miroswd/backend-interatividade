b5696169311d5420ec20d5f638d60391244ea670
miroswd <altamirsantos93@hotmail.com>
2020-09-17 19:06:45 -0300

    Avatar

--- src/index.js
+++ src/index.js
@@ -23,20 +23,28 @@ app.get('/', (request,response) => {
     const {id} = request.body
     console.log(id)
   let ala = []
+
+  
   T.get(
     `https://api.twitter.com/2/tweets?ids=${id}&tweet.fields=created_at&expansions=author_id&user.fields=created_at`,
     (error, data, response) => {
       for (let i =0; i < data.data.length; i++){     
         const {text} = data.data[i];
-         const {name, id} = data.includes.users[i];
-         const content = {name, text, userId:id};
-        ala.push(content)
+        const {name, username, id} = data.includes.users[i];
+        
+        T.get(`https://api.twitter.com/1.1/users/show.json?screen_name=${username}`,(error,data,response) => {
+          // data retornando tudo do usuário
+          const avatar_url = data.profile_image_url_https.replace('_normal','')
+          const content = {name, text, userId:id, avatar_url};
+          ala.push(content)
+        })
       }
       },
     );
+
+    
     
     setTimeout(() => {
-      console.log(ala)
       response.status(200).json(ala)
     }, 2000)
   } catch (error) {
@@ -46,6 +54,6 @@ app.get('/', (request,response) => {
 });
 
 
-app.listen(3030, () => {
+app.listen(3333, () => {
   console.log('Rodando MAOE')
 });
